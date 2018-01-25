180b036dc4e4928209399ac69c3ed9c0befc17e8
visualsuccess <scott@visualsuccess.com>
2018-01-25 14:43:43 -0600

    Updated Code

--- src/main/java/com/reservation/repository/RoomTypeRepository.java
+++ src/main/java/com/reservation/repository/RoomTypeRepository.java
@@ -1,5 +1,8 @@
 package com.reservation.repository;
 
+import java.util.Date;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -11,5 +14,9 @@ import com.reservation.models.RoomType;
 
 @RepositoryRestResource
 public interface RoomTypeRepository extends CrudRepository<RoomType, Integer>{
+	
+	@Query("SELECT rt FROM RoomType rt ") //WHERE rt.typeId = 1
+	public Iterable<RoomType> findAvaliableRooms (); //Date checkInDate, Date checkOutDate
+	
 	
 }
