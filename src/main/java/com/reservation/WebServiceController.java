115a2210b8672fd62f96862ad617c9dea1295d92
scott.kramer@illinoisjug.org <scott.kramer@illinoisjug.org>
2018-01-25 15:29:48 -0600

    webpage for the day

---
+++ src/main/java/com/reservation/WebServiceController.java
@@ -0,0 +1,27 @@
+package com.reservation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebServiceController {
+//	@GetMapping(value = "/availableRooms")
+//	public List<Rooms> getAvailableRooms() {
+//		// TODO Get it from service
+//		List<Rooms> rooms = getRooms();
+//		
+//		return rooms;
+//	}
+//	
+//	// REMOVE ME
+//	List<Rooms> getRooms() {
+//		List<Rooms> list = new ArrayList<Rooms>();
+//		Rooms room1 = new Rooms();
+//		list.add(room1);
+//		return list;
+//	}
+
+}
