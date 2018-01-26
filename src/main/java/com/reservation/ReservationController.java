c53197344a70154a5821f20469b1fe98e39821e6
visualsuccess <scott@visualsuccess.com>
2018-01-26 14:50:50 -0600

    Find available rooms

--- src/main/java/com/reservation/ReservationController.java
+++ src/main/java/com/reservation/ReservationController.java
@@ -1,10 +1,17 @@
 package com.reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.reservation.models.RoomType;
+import com.reservation.repository.RoomTypeRepository;
 
 @Controller
 public class ReservationController {
+	@Autowired
+	private RoomTypeRepository roomTypeRepository;
 	
 	@RequestMapping(value = "/")
 	public String home() {
@@ -12,7 +19,9 @@ public class ReservationController {
 	}
 	
 	@RequestMapping(value = "/availableRooms")
-	public String availableRooms() {
+	public String availableRooms(Model model) {
+//		Iterable<RoomType> roomTypes = roomTypeRepository.findAvaliableRooms();
+//		model.addAttribute("roomTypes", roomTypes);
 		return "availableRooms";
 	}
 }
